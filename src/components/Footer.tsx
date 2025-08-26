0aed3567234362468b7bde82a44836cfe7a1e5a1
sabir-salmani <sabir.salmani4567@gmail.com>
2025-08-26 21:32:50 +0530

    footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -949,7 +949,7 @@ const Footer: React.FC = () => {
 
       {/* Bottom Bar */}
       <div className="border-t border-gray-800">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <div className="max-w-7xl mx-auto px-4 sm:px-2 lg:px-8 py-1">
           <div className="flex flex-col md:flex-row items-center justify-between">
             <p className="text-gray-400 text-sm">
               © 2025 Qyuki AI. All rights reserved.
